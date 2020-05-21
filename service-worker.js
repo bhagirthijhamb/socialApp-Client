efe5e1e0bb2b507b334f97459fdfc5e287e33591
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-05-21 18:30:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/socialApp-Client/precache-manifest.6f91044eef8f3c962eca7d4163680c8f.js"
+  "/bhagirthijhamb/socialApp-Client.git/precache-manifest.d4d6e7ffcee4d1816340c0c6908f4bb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/socialApp-Client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bhagirthijhamb/socialApp-Client.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
