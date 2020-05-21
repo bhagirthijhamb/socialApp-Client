f8fb123e6865bc380cfa01dd8745a65450a1c926
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-05-21 18:20:00 -0400

    Updates

---
+++ precache-manifest.6f91044eef8f3c962eca7d4163680c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a5d965c1dd5051dcfc17358b171c64",
+    "url": "/socialApp-Client/index.html"
+  },
+  {
+    "revision": "01ad78e6377f002aa258",
+    "url": "/socialApp-Client/static/css/main.befdb12a.chunk.css"
+  },
+  {
+    "revision": "b239adbe6d7469897c31",
+    "url": "/socialApp-Client/static/js/2.582532d7.chunk.js"
+  },
+  {
+    "revision": "01ad78e6377f002aa258",
+    "url": "/socialApp-Client/static/js/main.65727989.chunk.js"
+  },
+  {
+    "revision": "f6f06a6ee7917820719d",
+    "url": "/socialApp-Client/static/js/runtime-main.ec55094b.js"
+  }
+]);
